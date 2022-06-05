e58f9d96504edda1af67990eb07b65d1388db79f
Dayton <emailaddr>
2022-06-04 21:56:38 -0500

    Able to isolate lip points of interest, work needs to be done to determine good relative lip positions

--- main.py
+++ main.py
@@ -8,8 +8,21 @@ import mediapipe as mp
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 mp_draw = mp.solutions.drawing_utils
 mpface_mesh = mp.solutions.face_mesh
-face_mesh = mpface_mesh.FaceMesh(max_num_faces = 2)
-draw_spec = mp_draw.DrawingSpec(thickness=1, circle_radius=1)
+face_mesh = mpface_mesh.FaceMesh(max_num_faces = 1)
+draw_spec = mp_draw.DrawingSpec(thickness=1, circle_radius=1, color=(255, 255, 0))
+
+lip_points = {
+    0 : [0, 0],
+    12 : [0, 0],
+    15 : [0, 0],
+    17 : [0, 0],
+    39 : [0, 0],
+    57 : [0, 0],
+    181 : [0, 0],
+    269 : [0, 0],
+    287 : [0, 0],
+    405 : [0, 0]
+}
 
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
@@ -26,22 +39,20 @@ while True:
 
     results = face_mesh.process(imgRGB)
 
-    if results.multi_face_landmarks:
+    #Gets the points of the face mesh but does not display the face mesh
+    if results.multi_face_landmarks: 
         for face_lms in results.multi_face_landmarks:
-            mp_draw.draw_landmarks(frame, face_lms, mpface_mesh.FACE_CONNECTIONS, draw_spec, draw_spec)
             for id, lm in enumerate(face_lms.landmark):
-                ih, iw, ic = frame.shape
-                x, y = int(lm.x*iw), int(lm.y*ih)
-
-    '''
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-    for (x, y, w, h) in faces:
-        cv.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
-    '''
-
-    cv.imshow('frame', frame)
+                if id in lip_points:
+                    ih, iw, ic = frame.shape
+                    x, y = int(lm.x*iw), int(lm.y*ih)   
+                    lip_points[id] = [x, y]
+    
+    #Place a dot for every lip articulation point
+    for id in lip_points:
+        frame = cv.circle(frame, (lip_points[id][0], lip_points[id][1]), 1, (255,255,0), 4)
+    
+    cv.imshow('test face mesh', frame)
     if cv.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
